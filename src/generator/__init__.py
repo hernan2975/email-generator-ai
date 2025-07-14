30da9044374bad291ae24223e5a020abc1c9763d
https://hll2975.github.io/M-Hoja-de-Vida-/Hernán-Luis-IT <56049876+hernan2975@users.noreply.github.com>
2025-07-14 08:06:10 -0300

    Create __init__.py

---
+++ src/generator/__init__.py
@@ -0,0 +1,6 @@
+
+from .prompts import EmailPromptTemplate
+from .api_client import OpenAIClient
+from .postprocessor import EmailPostProcessor
+
+__all__ = ["EmailPromptTemplate", "OpenAIClient", "EmailPostProcessor"]
